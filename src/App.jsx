698002d707c7df55d1db8c245466b5c71b0e2077
Darian <darianreyesromero@hotmail.com>
2023-04-01 20:27:38 -0600

    Commit con terminal de windows

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
 function App(){
-return <h1>Hola mundo, estoy subiendo un archivo desde la terminal</h1>
+return <h1>Hola mundo, estoy subiendo un archivo desde la terminal, pero ahora lo hice con windows</h1>
 }
 export default App
